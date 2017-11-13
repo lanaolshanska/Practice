e830632f88be60e75ca746c0d30671cfca5fd334
lanaolshanska <svitlana.olshanska@nure.ua>
2017-11-13 22:04:35 +0200

    Lesson2
    Using indexers, indexing an array within the specified limits.

---
+++ Lesson2/Lesson2/Indexer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class Indexer
+    {
+        int[] arr { get; set; }
+        int lowerIndex { get; set; }
+        int higherIndex { get; set; }
+
+        public Indexer(int[] arr, int lowerIndex, int higherIndex)
+        {
+            this.arr = arr;
+
+            if (IsCorrectRange() && IsNotOutOfRange())
+            {
+                this.lowerIndex = lowerIndex;
+                this.higherIndex = higherIndex;
+            }
+            else
+            {
+                this.lowerIndex = lowerIndex;
+                this.higherIndex = lowerIndex + arr.Length - 1;
+            }
+        }
+
+        public int this [int index]
+        {
+            get
+            {
+                if (IsCorrectIndex(index))
+                {
+                    return arr[index - lowerIndex];
+                }
+                else
+                {
+                    Console.WriteLine("Index is out of the specified range\n");
+                    return 0;
+                }
+            }
+
+            set
+            {
+                if (IsCorrectIndex(index))
+                {
+                    arr[index - lowerIndex] = value;
+                }
+                else
+                {
+                    Console.WriteLine("Index is out of the specified range\n");
+                }
+            }
+        }
+
+        private bool IsNotOutOfRange()
+        {
+            int indexRange = higherIndex - lowerIndex + 1;
+            int arrayRange = arr.Length;
+
+            return (indexRange <= arrayRange);
+        }
+
+        private bool IsCorrectRange()
+        {
+            return higherIndex > lowerIndex;
+        }
+
+        private bool IsCorrectIndex(int index)
+        {
+            return ((lowerIndex <= index) && (index <= higherIndex));
+        }
+    }
+}
---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            Indexer indexerArray1 = new Indexer(arr, 1, 5);
+
+            Console.WriteLine("Array with range {1, 5}:\n");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("indexerArray1[{0}] = {1}", i, indexerArray1[i]);
+            }
+
+            Console.WriteLine();
+
+
+            Indexer indexerArray2 = new Indexer(arr, -5, 1);
+
+            Console.WriteLine("Array with range {-5, -1}:\n");
+
+            for (int i = -5; i <= -1; i++)
+            {
+                Console.WriteLine("indexerArray2[{0}] = {1}", i, indexerArray2[i]);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to exit the program.");
+            Console.ReadKey();
+        }
+    }
+}
